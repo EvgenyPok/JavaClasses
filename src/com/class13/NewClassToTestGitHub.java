b2cf7a7fdb2f5c73946b01cd9402d7161d0311ba
EvgenyPok <oksijohn@gmail.com>
2019-03-28 22:02:29 -0400

    updated class 13 with Task 3

--- src/com/class13/NewClassToTestGitHub.java
+++ src/com/class13/NewClassToTestGitHub.java
@@ -3,8 +3,22 @@ package com.class13;
 public class NewClassToTestGitHub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello GitHub... are you watching me?");
+		// Task 1
+		String str=" Java is a programming language";
+		for (int i=str.length()-1; i>=0;i--) {
+		System.out.print(str.charAt(i));}
+		// Task 2
+		String str2="what is going on?";
+		System.out.println();
+		if (!str2.isEmpty()) {
+			if (str2.length()%2==1 && str2.length()>=3) {
+				System.out.println(str2.charAt(str2.length()/2));
+			}else {
+				System.out.println("Str2 has even number of chars or has less than 3 chars");
+			}
+		}else {
+			System.out.println("Str2 is empty"+str2.isEmpty());
+		} 
 	}
 
 }
---
+++ src/com/class13/StringsTask3.java
@@ -0,0 +1,31 @@
+package com.class13;
+
+import java.util.Scanner;
+
+public class StringsTask3 {
+
+	public static void main(String[] args) {
+		// Task 3 Write a program that reads two people’s first names and if they expecting boy or girl? Based on the input suggests a name for a baby:
+		Scanner input=new Scanner (System.in);
+		String mom,dad,babySex,babyName;
+		System.out.print("Enter Mom's name: ");
+		mom=input.nextLine();
+		System.out.print("Enter Dad's name: ");
+		dad=input.nextLine();
+		System.out.println("boy or girl?");
+		babySex=input.nextLine();
+		switch (babySex) {
+		case "boy":
+			babyName=dad.substring(0,3)+mom.substring(mom.length()-2,mom.length());
+			System.out.println("Suggested baby name: "+babyName.toUpperCase());
+			break;
+		case "girl":
+			babyName=mom.substring(0,2)+dad.substring(dad.length()-3,dad.length());
+			System.out.println("Suggested baby name: "+babyName.toUpperCase());
+			break;
+		default:
+			System.out.println("it's not a boy nor girl");
+		}
+	}
+
+}
