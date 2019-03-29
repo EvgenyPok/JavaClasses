df22a52a87bf306712034911e50afa4c34d16377
EvgenyPok <oksijohn@gmail.com>
2019-03-28 21:11:01 -0400

    Added class com.class13

---
+++ src/com/class13/NewClassToTestGitHub.java
@@ -0,0 +1,10 @@
+package com.class13;
+
+public class NewClassToTestGitHub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello GitHub... are you watching me?");
+	}
+
+}
